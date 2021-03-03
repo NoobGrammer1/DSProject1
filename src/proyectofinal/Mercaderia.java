6e1c569e87ca8f3b5cdc6e7610f79b5192fd75b5
stali <stali@Stalin-PC-Win10>
2021-03-03 14:36:01 -0500

    implementación de variables y metodos en Clase Mercaderia

--- src/proyectofinal/Mercaderia.java
+++ src/proyectofinal/Mercaderia.java
@@ -11,4 +11,63 @@ package proyectofinal;
  */
 public class Mercaderia {
     
+private String nombre;
+private int codigo;
+private int cantidad;
+private double precio;
+
+//contructores
+
+    public Mercaderia() {
+    }
+
+    public Mercaderia(String nombre, int codigo, int cantidad, double precio) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+//metodos set
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    //metodos get
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+    //metodo toString
+
+    @Override
+    public String toString() {
+        return "Mercaderia{" + "nombre=" + nombre + ", codigo=" + codigo + ", cantidad=" + cantidad + ", precio=" + precio + '}';
+    }
+    
+
 }
